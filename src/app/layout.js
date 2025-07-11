027d1ceba664b701646ec819e44343954ce44e81
manojinsanger <manoj@insanger.com>
2025-07-11 20:23:22 +0530

    added a licon

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logoWhiteHeaven.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
